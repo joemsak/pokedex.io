f9c5e5f7e30e96b6f0c97eeb43f0c2aecd1df21e
Joe Sak <joe@joesak.com>
2021-04-16 16:14:03 -0500

    Add secure auth token to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   friendly_id :slug, use: [:slugged, :history]
 
   has_secure_password
+  has_secure_token :auth_token, length: 36
 
   validates :email, uniqueness: true
-
 end
