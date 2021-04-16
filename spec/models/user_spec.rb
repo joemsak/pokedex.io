e4405a949de50f00c38fdbdfbb80a08cc021ee4a
Joe Sak <joe@joesak.com>
2021-04-16 16:25:57 -0500

    Get more specific

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,11 @@ RSpec.describe User, type: :model do
   describe "#email" do
     it "must be unique" do
       user = create(:user, email: "hello@world.com")
-      expect(build(:user, email: "hello@world.com")).not_to be_valid
+      user2 = build(:user, email: "hello@world.com")
+
+      expect(user2).not_to be_valid
+      expect(user2.errors.size).to be 1
+      expect(user2.errors[:email]).to eq(["has already been taken"])
     end
   end
 end
