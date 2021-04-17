9b69cb269f97eaf0d68ca5a7eac7f496ea31ca61
Joe Sak <joe@joesak.com>
2021-04-16 23:44:37 -0500

    Load locale files from subdirs

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,10 @@ module App
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.i18n.load_path += Dir[
+      Rails.root.join('config', 'locales', '**', '*.{rb,yml}')
+    ]
+
     config.generators do |g|
       g.system_tests = nil
       g.test_framework :rspec
