579ca8fcd05ff366c6e025ce152b761c72134c96
Joe Sak <joe@joesak.com>
2021-04-18 21:55:49 -0500

    Setup sidekiq

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,8 @@ module App
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.active_job.queue_adapter = :sidekiq
+
     config.i18n.load_path += Dir[
       Rails.root.join('config', 'locales', '**', '*.{rb,yml}')
     ]
