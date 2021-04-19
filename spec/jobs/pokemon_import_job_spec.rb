93b940a1b692c3214daaf60af262d82d3c80f9ac
Joe Sak <joe@joesak.com>
2021-04-18 22:09:47 -0500

    Add import job specs

--- spec/jobs/pokemon_import_job_spec.rb
+++ spec/jobs/pokemon_import_job_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe PokemonImportJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#perform" do
+    let(:data) { File.read("./spec/support/api_data/pokemon_detail.json") }
+
+    before do
+      allow(Net::HTTP).to receive(:get_response)
+        .with(URI(result["url"]))
+        .and_return(double(:response, read_body: data))
+    end
+
+    context "for a new URL" do
+      let(:result) { { "url" => "https://example.org/api/v2/pokemon/1" } }
+
+      it "creates a pokemon import" do
+        expect {
+          PokemonImportJob.new.perform(result)
+        }.to change {
+          PokemonImport.count
+        }.from(0).to(1)
+
+        import = PokemonImport.last
+
+        expect(import.url).to eq(result["url"])
+        expect(import.data).to eq(JSON.parse(data))
+      end
+    end
+
+    context "for a matching URL" do
+      let(:import) { create(:pokemon_import) }
+      let(:result) { { "url" => import.url } }
+
+      it "updates existing pokemon imports" do
+        expect {
+          PokemonImportJob.new.perform(result)
+        }.not_to change {
+          PokemonImport.count
+        }.from(1)
+
+        expect(import.reload.data).to eq(JSON.parse(data))
+      end
+    end
+  end
 end
