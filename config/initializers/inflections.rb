ed6d4c50b85e0e82685a9fa7aa2f500d2e5aacd7
Joe Sak <joe@joesak.com>
2021-04-22 14:02:04 -0500

    Only need the irregular inflection

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -4,10 +4,7 @@
 # are locale specific, and you may define rules for as many different
 # locales as you wish. All of these examples are active by default:
 ActiveSupport::Inflector.inflections(:en) do |inflect|
-  inflect.plural /^(pokemon)$/i, '\1'
-  inflect.singular /^(pokemon)$/i, '\1'
-#   inflect.irregular 'person', 'people'
-#   inflect.uncountable %w( fish sheep )
+  inflect.irregular "pokemon", "pokemon"
 end
 
 # These inflection rules are supported but not enabled by default:
